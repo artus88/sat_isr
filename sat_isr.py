ad6e5b73e039ce7b4ff88e42a796c54a176c4941
Arturo Escobar <artus88@gmail.com>
2021-09-15 15:30:45 -0500

    The logic was changed to POO paradigm

--- sat_isr.py
+++ sat_isr.py
@@ -1,91 +1,91 @@
 import matplotlib.pyplot as plt
 import csv
 
-def read(file_name):
-    with open(file_name,"r",encoding="utf-8") as csvfile:
-        limites=csv.DictReader(csvfile)
-        limites_dic=list(limites)
-    return limites_dic
 
-def cal_isr (ingreso_grav, limites):
+class salary_range():
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
 
-    for i in limites:
-        if float(i["limite_i"]) < ingreso_grav < float(i["limite_s"]):
-            isr_f = float(i["cuota_f"]) + ((ingreso_grav-float(i["limite_i"]))*(float(i["tasa"])/100))            
-    return isr_f
+class isr_object ():
+    def __init__(self, year, rango):
+        self.year = year
+        self.rango = rango
+        self._limits_m = self.limits(period="mensual")
+        self._limits_a = self.limits(period="anual")
+        self._range_list = self.range_list()
 
-def cal_isr_2 (ingreso_grav, limite_csv):
-
-    for row in limite_csv:
-        if float(row["limite_i"]) < ingreso_grav < float(row["limite_s"]):
-            isr_f = float(row["cuota_f"]) + ((ingreso_grav-float(row["limite_i"]))*(float(row["tasa"])/100))            
-    return isr_f
-
-def anual_mensual(ingreso_grav):
-    return (ingreso_grav*12.5)
-
-def anual_quincenal(ingreso_grav):
-    return (ingreso_grav/2)
-
-def tasa_t (ingreso_grav, limites):
-    isr_f=cal_isr(ingreso_grav,limites)
-    tasa_total= (isr_f*100)/ingreso_grav
-    return tasa_total
-
-def plot_tasa(a,b):
-        
-    file_name_monthy= "./mensual_2021.csv"
-    file_name_annual= "./anual_2021.csv"
-    file_name_quincenal= "./quincenal_2021.csv"
-
-    limites_mon = read(file_name_monthy)
-    limites_anual = read (file_name_annual)
-    limites_quincenal = read (file_name_quincenal)
-
-    list_isr=[cal_isr(i,limites_mon) for i in range(a,b)]
-    list_tasa_mensual=[tasa_t(i,limites_mon) for i in range(a,b) ]
-    list_tasa_anual=[tasa_t(anual_mensual(i),limites_anual) for i in range(a,b) ]
-    list_tasa_quincenal=[tasa_t(anual_quincenal(i),limites_quincenal) for i in range(a,b) ]
-
-    print("Esta grafica muestra como crece el porcentaje de ISR con respecto al aumento de salario")
-    plt.plot(range(a,b),list_tasa_mensual, label='Con tablas Mensuales')
-    plt.plot(range(a,b),list_tasa_anual, label='Con tablas Anuales(contando 15 dias de Aguinaldo)')
-    plt.legend()
-    # plt.plot(range(a,b),list_tasa_quincenal)
-    plt.title('Esta grafica muestra \n como crece el porcentaje de ISR retenido con respecto al aumento de salario ')
-    plt.yscale('linear')
-    plt.ylabel('Tasa de impuestos en mexico % ')
-    plt.xlabel('Sueldo Mensual (MXN)')
-    plt.grid()
-    plt.show()
-
-def plot_sueldo(a,b):
+    # @property
+    # def limits(self):
+    #     return self._limits
+    
+    # @limits.setter
+    def limits(self, period):
+        file_name="./"+period+"_"+self.year+".csv"
+        with open(file_name,"r",encoding="utf-8") as csvfile:
+            limites=csv.DictReader(csvfile)
+            limites_dic=list(limites)
+        return limites_dic
 
     
-    file_name_monthy= "./mensual_2021.csv"
-    file_name_annual= "./anual_2021.csv"
-    file_name_quincenal= "./quincenal_2021.csv"
+    # @property
+    # def range_list(self):
+    #     return self._range_list
+    
+    # @limits.setter
+    def range_list(self):
+        list1=[i for i in range(self.rango.a,self.rango.b)]
+        return list1
+        
+        
 
-    limites_mon = read(file_name_monthy)
-    limites_anual = read (file_name_annual)
-    limites_quincenal = read (file_name_quincenal)
+    def cal_isr (self, ingreso_grav, limites):
+        for i in limites:
+            if float(i["limite_i"]) < ingreso_grav < float(i["limite_s"]):
+                isr_f = float(i["cuota_f"]) + ((ingreso_grav-float(i["limite_i"]))*(float(i["tasa"])/100))            
+        return isr_f
 
-    list_isr_mensual=[cal_isr(i,limites_mon) for i in range(a,b)]
-    list_isr_anual=[cal_isr(anual_mensual(i),limites_anual)/12.5 for i in range(a,b) ]
 
+    def anual_mensual(self,ingreso_grav):
+        return (ingreso_grav*12.5)
 
-    print("Esta grafica muestra como crece la retención de ISR con respecto al aumento de salario")
-    plt.plot(range(a,b),list_isr_mensual, label='Con tablas Mensuales')
-    plt.plot(range(a,b),list_isr_anual, label='Con tablas Anuales(contando 15 dias de Aguinaldo)')
-    plt.legend()
-    # plt.plot(range(a,b),list_tasa_quincenal)
-    plt.title('Esta grafica muestra \n como crece la retención de ISR con respecto al aumento de salario')
-    plt.yscale('linear')
-    plt.ylabel('Tasa de impuestos en mexico % ')
-    plt.xlabel('Sueldo Mensual (MXN)')
-    plt.grid()
-    plt.show()
+    def anual_quincenal(ingreso_grav):
+        return (ingreso_grav/2)
 
+    def tasa_t (self, ingreso_grav, limites):
+        isr_f=self.cal_isr(ingreso_grav,limites)
+        tasa_total= (isr_f*100)/ingreso_grav
+        return tasa_total
+
+    def plot_tasa(self):
+        
+        list_tasa_mensual=[self.tasa_t(i,self._limits_m) for i in self._range_list ]
+        list_tasa_anual=[self.tasa_t(self.anual_mensual(i),self._limits_a) for i in self._range_list ]
+        print("Esta grafica muestra como crece el porcentaje de ISR con respecto al aumento de salario")
+        plt.plot(self._range_list,list_tasa_mensual, label='Con tablas Mensuales')
+        plt.plot(self._range_list,list_tasa_anual, label='Con tablas Anuales(contando 15 dias de Aguinaldo)')
+        plt.legend()
+        plt.title('Crecimiento de la tasa de ISR retenido con respecto al aumento de salario ')
+        plt.yscale('linear')
+        plt.ylabel('Tasa de impuestos en mexico % ')
+        plt.xlabel('Sueldo Mensual (MXN)')
+        plt.grid()
+        plt.show()
+    
+    def plot_isr(self):
+        
+        list_tasa_mensual=[self.cal_isr(i,self._limits_m) for i in self._range_list ]
+        list_tasa_anual=[self.cal_isr(self.anual_mensual(i),self._limits_a)/12.5 for i in self._range_list ]
+        print("Esta grafica muestra como crece la retención de ISR con respecto al aumento de salario")
+        plt.plot(self._range_list,list_tasa_mensual, label='Con tablas Mensuales')
+        plt.plot(self._range_list,list_tasa_anual, label='Con tablas Mensuales')
+        plt.legend()
+        plt.title('Crecimiento de la retención de ISR con respecto al aumento de salario')
+        plt.yscale('linear')
+        plt.ylabel('Impuesto retenido por el SAT (MXN) ')
+        plt.xlabel('Sueldo Mensual (MXN)')
+        plt.grid()
+        plt.show()
 
 def input_function():
 
@@ -101,18 +101,17 @@ def input_function():
     assert b.isnumeric  and int(b) > int(a), "Opciones nos validas"
     b = int(b)
     if option == "a":
-        plot_tasa(a,b)
+        salary_range1=salary_range(a,b)
+        isr_mexico=isr_object('2021',salary_range1)
+        isr_mexico.plot_tasa()
+
     elif option == "b":
-        plot_sueldo(a,b)
-
-
+        salary_range1=salary_range(a,b)
+        isr_mexico=isr_object('2021',salary_range1)
+        isr_mexico.plot_isr()
 
 def run():
-    #ingreso_grav = float(input("Cual es tu ingreso Gravable? : "))
-
     input_function()
-
-
 
 if __name__ == '__main__':
     run()
